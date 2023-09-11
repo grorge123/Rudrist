fb552921f180fb47f8010520dcc8fe8e9735c4bf
grorge <g97116@yahoo.com>
2023-09-11 21:51:23 +0800

    feat: add rust porject skeleton

---
+++ target/debug/deps/hello_world-83c875698bcbac5d.d
@@ -0,0 +1,5 @@
+/home/grorge/Desktop/Rust/hello-world/target/debug/deps/hello_world-83c875698bcbac5d.rmeta: src/main.rs
+
+/home/grorge/Desktop/Rust/hello-world/target/debug/deps/hello_world-83c875698bcbac5d.d: src/main.rs
+
+src/main.rs:
